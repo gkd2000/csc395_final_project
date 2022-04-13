0a7b970ca90ceda935213c704caa521c8243a8f0
Paul Lu <luhanfeng1106@sina.com>
2022-04-12 19:35:29 -0500

    simplify init and hello

--- helloworld/helloworld.c
+++ helloworld/helloworld.c
@@ -7,33 +7,9 @@
 
 
 void _start() {
-  // Issue a write system call
 
-  // // int * str = (int*)0x5723842093;
-  // // mmap(str, sizeof(int)*10, 0, 0, 0, 0);
-  // // *str = 5;
-  // syscall(SYS_write, 1, "Hello world!\n", 13);
-  // // syscall(SYS_write, 1, "Hello world!\n", 13);
-  // write(1, "Hello world!\n", 13);
 
-  // syscall(SYS_write, 1, "Hello world!\n", 13);
-    // Loop forever
-  // for(;;){}
-
-  // char* test_page = (char*)0x400000000;
-  // test_page[0] = 'h';
-  // test_page[1] = 'e';
-  // test_page[2] = 'l';
-  // test_page[3] = 'l';
-  // test_page[4] = 'o';
-  // test_page[5] = '\n';
-  // syscall(SYS_write, 1, test_page, 6);
-
-  // char buffer1[10];
-  // read(0, buffer1, 9);
-
-  exec("shell", NULL);
+  printf("Hello world %d\n", 12342);
   // Loop forever
   exit(0);
-  for(;;){}
 }
